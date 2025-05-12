5ae7292aa1f76206dc92c29e07c47542aea47ecc
micheledidonna01 <micheledidonna337@gmail.com>
2025-05-12 16:30:36 +0200

    add: cors middleware

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 const port = process.env.PORT || 3005;
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const errorsHandler = require('./middlewares/errorsHandler.js');
 const notFound = require('./middlewares/notFound.js');
-
-//body parser
-app.use(express.json());
 
 
 //connection to routers
 const movieRouter = require('./routers/movies.js');
 
+//cors middleware
+app.use(cors({
+    origin: process.env.FE_APP
+}));
+
 //static middleware
 app.use(express.static('public'));
+
+//body parser
+app.use(express.json());
+
 
 //route to homepage
 app.get('/', (req, res)=>{
     res.send('Benvenuto nella homepage');
 });
 
-//route of moviese
+//route of movies
 app.use('/api/movies', movieRouter);
 
 
